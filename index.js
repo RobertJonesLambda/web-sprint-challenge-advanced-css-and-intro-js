c05e289366d24c38f0a2711b74ef8fec1a89cc0e
HolyMustard <RobertJones117@gmail.com>
2020-08-29 13:50:11 -0400

    css and tasks 1-3

--- index.js
+++ index.js
@@ -208,10 +208,14 @@ const artists = [
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
 
-
+console.log(artists[0].name)
+console.log(artists[2].bio)
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
+// let dough = artists.splice([8].name, "Vincent Van Gough")
+
+// console.log(artists[8].name)
 
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
@@ -223,17 +227,23 @@ const artists = [
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
 function getArtistByIndex(array, index) {
-    /* code here */
+     let artistStatment = `The artist at ${index} is ${artists[0].name}`
   }
-  
+console.log(getArtistByIndex(artists, 0))    
   /**
 
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/* Code here */){
-
-  /* Code here */
+function get20s(arr, index){
+    //parseInt  to convert years to integers
+    for (let i=0; i < arr.length; i++){
+      arr[i] = parseInt(arr[i], "years")
+    }
+    //for loop and if to find artists active
+    for (let i=0; i < arr.length; i++){
+       if (arr[i] >= )
+    }  
 
 }
 
@@ -248,8 +258,10 @@ function get20s(/* Code here */){
  * 
  * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.  
 */
-function removeArtist(/*code here*/) {
-    /* code here */
+function removeArtist(arr, index) {
+    
+
+    return array.length
   }
   
  
